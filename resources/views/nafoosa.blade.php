2288c92450ac563fb43801ccd101db756712bc98
Sanhory <elrayah.alsanhory@gmail.com>
2018-09-02 15:56:58 +0200

    first day

--- resources/views/nafoosa.blade.php
+++ resources/views/nafoosa.blade.php
@@ -4,6 +4,15 @@
     <title>Nafoosa</title>
 </head>
 <body>
+hi there
 <p style='background-color:yellow'>Wlecome {{$name}} your {{$age}} to our first view,</p>
 </body>
 </html>
+
+<!--
+    resources/views/nafoosa.blade.php
+
+    Route::get('/hi',function(){
+    return view('nafoosa');
+    });
+-->
