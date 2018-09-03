f0f7f2df11276768f8cd613b406e4d40f41c2380
Sanhory <elrayah.alsanhory@gmail.com>
2018-09-03 11:13:35 +0200

    ourdata change

--- resources/views/ourdata.blade.php
+++ resources/views/ourdata.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>OUr data</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+ 
 </head>
 <body>
 <pre>
